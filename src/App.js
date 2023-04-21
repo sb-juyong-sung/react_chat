76a0201e37e25e47196e4161495e1c52612e7659
Juyong Sung <juyong.sung@juyong.sung-MacBookPro>
2023-04-21 14:30:13 +0900

    feature: make up id, pw, login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { useState } from 'react';
 import Chat from './chat_page';
+import Login from './login';
 
 export default function App() {
+  const [isLogined, setIsLogined] = useState(false)
+
   return (
-    <Chat />
+    <Login />
   );
 }
 
---
+++ src/login.js
@@ -0,0 +1,18 @@
+export default function Login() {
+    return (
+        <>
+            <div>
+                Sendbird
+            </div>
+            <div>
+                ID : <input type="text"></input>
+            </div>
+            <div>
+                PW : <input type="text"></input>
+            </div>
+            <div>
+                <button>login</button>
+            </div>
+        </>
+    );
+}
