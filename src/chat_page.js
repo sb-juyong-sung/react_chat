f7b4b3d3b2eec3ae7da95594d1b27d4c82b85981
Juyong Sung <juyong.sung@juyong.sung-MacBookPro>
2023-04-21 13:52:14 +0900

    feature: divide page left(list) and right(chat)

---
+++ src/chat_page.js
@@ -0,0 +1,18 @@
+import './chat_page.css';
+
+export default function Chat() {
+    return (
+        <div>
+            <div className="align-left">
+                <h1>"Channel Name"</h1>
+                <hr></hr>
+            </div>
+            <div className="align-right">
+                <h1>"Channel Name"</h1>
+                <hr></hr>
+            </div>
+        </div>
+    );
+}
+
+
