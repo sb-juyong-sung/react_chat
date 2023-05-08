d435cb29220a558aeff67bad7897b30a9c2cfdb6
sb-juyong-sung <juyong.sung@sendbird.com>
2023-05-08 10:53:46 +0900

    fix: change type of user id

--- src/login.js
+++ src/login.js
@@ -6,17 +6,17 @@ import { OpenChannelModule, SendbirdOpenChat } from '@sendbird/chat/openChannel'
 
 
 export default function Login({isLogined, setIsLogined}) {
-
     function checkCredential() {
         const id_value = document.getElementById('id').value;
         const pw_value = document.getElementById('password').value;
         if (id_value === 'send' && pw_value === 'bird') {
             connectServer(id_value);
             setIsLogined(true);
+
         }
     }   
 
-    const connectServer = async ({id_value}) => {
+    const connectServer = async (id_value) => {
         const sb = SendbirdChat.init({
             appId: "AF724953-484C-4A31-A559-60D67D914C0A",
             modules: [
@@ -24,7 +24,6 @@ export default function Login({isLogined, setIsLogined}) {
             ],
           }
           );
-          
           const user = await sb.connect(id_value);
     }
 
