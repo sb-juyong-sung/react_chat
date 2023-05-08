ae2f3a4b012eafba66936b540256a2a89a3c5a92
sb-juyong-sung <juyong.sung@sendbird.com>
2023-05-08 10:05:16 +0900

    feat: init sdk and connect to server

--- src/chat_page.js
+++ src/chat_page.js
@@ -1,4 +1,16 @@
 import './chat_page.css';
+import SendbirdChat from '@sendbird/chat';
+import { OpenChannelModule, SendbirdOpenChat } from '@sendbird/chat/openChannel';
+
+const sb = SendbirdChat.init({
+  appId: "AF724953-484C-4A31-A559-60D67D914C0A",
+  modules: [
+      new OpenChannelModule(),
+  ],
+}
+);
+
+const user = await sb.connect("send");
 
 export default function Chat() {
     return (
