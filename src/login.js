74ceba60de3a44e12c6e4bfbc361ef58ac4e7455
sb-juyong-sung <juyong.sung@sendbird.com>
2023-05-10 11:10:28 +0900

    feat: Set nickname when connect to sb server

--- src/login.js
+++ src/login.js
@@ -5,20 +5,25 @@ import { GroupChannelModule } from '@sendbird/chat/groupChannel';
 
 
 
-export default function Login({sb, isLogined, setIsLogined}) {
+
+export default function Login({ sb, isLogined, setIsLogined }) {
     function checkCredential() {
         const id_value = document.getElementById('id').value;
-        const pw_value = document.getElementById('password').value;
-        if (pw_value === 'bird') {
-            connectServer(id_value);
-            setIsLogined(true);
+        const ni_value = document.getElementById('nickname').value;
 
-        }
-    }   
-
-    const connectServer = async (id_value) => {
-          const user = await sb.connect(id_value);
+        connectServer(id_value, ni_value);
+        setIsLogined(true);
     }
+
+    const connectServer = async (id_value, ni_value) => {
+        await sb.connect(id_value);
+        await sb.setChannelInvitationPreference(true);
+        const UserUpdateParams = {};
+        UserUpdateParams.nickname = ni_value;
+        await sb.updateCurrentUserInfo(UserUpdateParams);
+    }
+
+
 
     function clickEnter(e) {
         if (e.key === 'Enter') {
@@ -28,20 +33,20 @@ export default function Login({sb, isLogined, setIsLogined}) {
 
 
     return (
-        <>  
+        <>
             <img src="/Sendbird_Symbol_RGB.png"
-            width={300}
-            height={300}
-            alt = 'Sendbird Symbol'></img>
-   
+                width={300}
+                height={300}
+                alt='Sendbird Symbol'></img>
+
             <div className="logo">
                 Sendbird
             </div>
             <div className='align'>
-                ID : <input id = 'id' type="text" onKeyPress={clickEnter}></input>
+                ID : <input id='id' type="text" onKeyPress={clickEnter}></input>
             </div>
             <div className='align'>
-                PW : <input id = 'password' type="text" onKeyPress={clickEnter}></input>
+                NICKNAME : <input id='nickname' type="text" onKeyPress={clickEnter}></input>
             </div>
             <div className='align'>
                 <button onClick={() => checkCredential()}>login</button>
