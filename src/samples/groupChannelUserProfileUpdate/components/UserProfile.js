3cda0f7c0e446711d426e1a18d4270119f70e447
sb-juyong-sung <juyong.sung@sendbird.com>
2023-05-22 10:09:45 +0900

    feat: Update user profile

--- src/samples/groupChannelUserProfileUpdate/components/UserProfile.js
+++ src/samples/groupChannelUserProfileUpdate/components/UserProfile.js
@@ -1,16 +1,26 @@
 import '../pages/ChatPage/ChatPage.css';
+import { useState } from 'react';
 
 function UserProfile({ sb, profileUpdateState, setProfileUpdateState }) {
+    
+    const [file, setFile] = useState(null);
 
     function handleUpdateProfile() {
         setProfileUpdateState(false);
         console.log(profileUpdateState);
     }
 
-    async function handleUpdateNickname(nick) {
-        const params = {nickname: nick,}
+    async function handleUpdateUser(nick, file) {
+        const params = { 
+            nickname: nick,
+            profileImage: file,
+         };
         const user = await sb.updateCurrentUserInfo(params);
     };
+
+    async function handleUploadProfile(e) {
+        setFile(e.currentTarget.files[0]);
+    }
 
 
     if (profileUpdateState) {
@@ -19,10 +29,17 @@ function UserProfile({ sb, profileUpdateState, setProfileUpdateState }) {
                 <div className='overlay-content'>
                     <h2>Profile</h2>
                     <div>
+                        Img:
+                        <input
+                            type='file'
+                            onChange={handleUploadProfile}>
+                        </input>
+                    </div>
+                    <div>
                         Nickname:
                         <input id='changeNickname' type="text"></input>
                     </div>
-                    <button onClick={() => handleUpdateNickname(document.getElementById('changeNickname').value)}>Update</button>
+                    <button onClick={() => handleUpdateUser(document.getElementById('changeNickname').value, file)}>Update</button>
                     <button onClick={handleUpdateProfile}>Cancel</button>
                 </div>
             </div>
